9d636408e6feead3e46ee26f987446fdd3a60918
edwarddegrano <degrano@sheridancollege.ca>
2019-05-23 09:48:42 -0400

    new

---
+++ src/studentdemoweek1/Student.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package studentdemoweek1;
+
+/**
+ * This class models students.
+ * Student have a name and a studentID and a program
+ * @author dancye, 2019 edited May 23rd in class
+ */
+public class Student 
+{
+    private String name;
+    private String studentID;
+    private String program = "Computer Programmer" ;
+    private boolean isFullTime = true;
+    
+    public Student()
+    {
+        //intetionally left blank
+    }
+    /**
+     * The setter for name
+     */
+    public void setName(String givenName)
+    {
+        name = givenName;
+    }
+    
+    /**
+     * The getter for name
+     */
+    
+    public String getName()
+    {
+        return name;
+    }
+
+    /**
+     * @return the studentID
+     */
+    public String getStudentID() {
+        return studentID;
+    }
+
+    /**
+     * @param studentID the studentID to set
+     */
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+
+    /**
+     * @return the program
+     */
+    public String getProgram() {
+        return program;
+    }
+
+    /**
+     * @param program the program to set
+     */
+    public void setProgram(String program) {
+        this.program = program;
+    }
+
+    /**
+     * @return the isFullTime
+     */
+    public boolean isIsFullTime() {
+        return isFullTime;
+    }
+
+    /**
+     * @param isFullTime the isFullTime to set
+     */
+    public void setIsFullTime(boolean setFullTime) {
+        isFullTime = setFullTime;
+    }
+}
